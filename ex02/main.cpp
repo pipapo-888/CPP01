754a335603865ab200d97a50158abf4e27d55201
pipapo888 <kazuya2002uk@gmail.com>
2026-07-15 13:13:11 +0900

    ex02 完

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,10 +6,24 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 19:23:43 by knomura           #+#    #+#             */
-/*   Updated: 2026/07/12 19:36:01 by knomura          ###   ########.fr       */
+/*   Updated: 2026/07/15 13:09:01 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
 int main()
 {
+	std::string str = "HI THIS IS BRAIN";
+	std::string *p = &str;
+	std::string &ref = str;
+
+	std::cout << &str << std::endl
+			  << p << std::endl
+			  << &ref << std::endl;
+			  
+	std::cout << str << std::endl
+			  << *p << std::endl
+			  << ref << std::endl;
 }
